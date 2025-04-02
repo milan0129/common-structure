69e96d748408175542a5f7a192391a6c283623ef
Milan G <milang@hyperlinkinfosystem.net.in>
2025-04-02 12:01:55 +0530

    demo project

--- backend/common/constants.js
+++ backend/common/constants.js
@@ -1,13 +1,13 @@
 let Globals = {
 
-    'API_KEY': process.env.API_KEY,
-    'KEY': process.env.KEY,
-    'IV': process.env.IV,
-    'EMAIL_ID': process.env.EMAIL_ID,
-    'EMAIL_PASSWORD': process.env.EMAIL_PASSWORD,
+    'API_KEY': 'task_1234567890',
+    'KEY': 'HfDfIfcY8fkm5NCPtJJj0ZBj8kzXMwHa',
+    'IV': 'HfDfIfcY8fkm5NCP',
+    'EMAIL_ID': 'gmr840597@gmail.com',
+    'EMAIL_PASSWORD': 'dyca yebf lahf qads',
     //ERROR CODE
     'FAILED': 0,
-    'SUCCESS': 1,
+    'SUCCESS': 1,                                               
     'NO_DATA_FOUND': 2,
     'ACCOUNT_INACTIVE': 3,
     'AUTH_ROLE': 4,
--- backend/config/database.js
+++ backend/config/database.js
@@ -1,11 +1,10 @@
 var mysql = require('mysql');
 
 var con = mysql.createPool({
-    host : process.env.DATABASE_HOST,
-    user : process.env.DATABASE_USER,
-    password : process.env.DATABASE_PASSWORD,
-    database : process.env.DATABASE_NAME,
-    port: process.env.DATABASE_PORT,
+    host : 'localhost',
+    user : 'root',
+    password : '',
+    database : 'task_',
     dateStrings : 'date'
 });
 module.exports = con;
